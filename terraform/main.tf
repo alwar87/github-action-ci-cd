2cab133a476fb25045a4ab61805fdc8e64219796
alwar87 <alwar87@gmail.com>
2026-02-10 12:50:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,38 @@
+resource "google_container_cluster" "primary" {
+  name     = "primary-gke"
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  addons_config {
+    http_load_balancing {
+      disabled = false
+    }
+  }
+
+  monitoring_config {
+    managed_prometheus {
+      enabled = true
+    }
+  }
+
+  network    = var.network
+  subnetwork = var.subnetwork
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
 
